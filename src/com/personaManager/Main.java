b6bd452f5b926cfa085817995701c57aad19073e
Yinnohs <joistleon@gmail.com>
2022-02-03 11:27:05 +0000

    added changes needed in point 8

--- src/com/personaManager/Main.java
+++ src/com/personaManager/Main.java
@@ -12,14 +12,25 @@ public class Main {
         jose.setBirthDate(LocalDate.of(1998,01,25));
         jose.setEmail("jose@jose.com");
         jose.setDni("79410692H");
-        System.out.println("------------ INFORMACION DE USUSARIO ------------");
-        System.out.println("El dni es: " + jose.getDni());
-        System.out.println("Tu fecha de nacimiento es: " + jose.getBirthDate());
-        System.out.println("Tu email es: " + jose.getEmail());
-        System.out.println("Tu nombre es:" + jose.getName());
-        System.out.println("Tu apellido es: "+jose.getSurName());
-        System.out.println("Tu edad es: " +jose.showAge());
-        System.out.println("------------ INFORMACION DE USUSARIO ------------");
 
+        printpersonInformation(jose);
+
+        jose.setName("Manuel Alejandro");
+        jose.setSurName("Garrido");
+
+        System.out.println("\n");
+
+        printpersonInformation(jose);
+    }
+
+    static void printpersonInformation(Person person){
+        System.out.println("------------ INFORMACION DE USUSARIO ------------");
+        System.out.println("El dni es: " + person.getDni());
+        System.out.println("Tu fecha de nacimiento es: " + person.getBirthDate());
+        System.out.println("Tu email es: " + person.getEmail());
+        System.out.println("Tu nombre es: " + person.getName());
+        System.out.println("Tu apellido es: "+person.getSurName());
+        System.out.println("Tu edad es: " +person.showAge());
+        System.out.println("------------ INFORMACION DE USUSARIO ------------");
     }
 }
